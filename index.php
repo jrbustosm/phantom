48a750ce8f5fbda74db0a3229e961f4440833a5c
jrbustosm <jrbustosm@gmail.com>
2013-05-06 17:26:15 -0500

    prueba de url limpias

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
 
+
+function get_url() {
+  $parametros = array();
+  $url = parse_url($_SERVER['REQUEST_URI']);
+  foreach(explode("/", $url['path']) as $p)
+    if ($p!='') $parametros[] = $p;
+  return $parametros;
+}
+
+print_r($_GET);
+print_r(get_url());
+
+
