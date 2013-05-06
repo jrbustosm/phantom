673280a937be2c8bd8d90aebed1097aca1dfe183
Brian Quilaguy <black-5791@hotmail.com>
2013-05-06 08:10:16 -0500

    conf.php

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+#configuracion basica
+$_TITULO = "phantom";
+$_EMAIL = "foo@server";
+#configuracion Base de datos
+$_BDMOTOR = "SQLLITE";
+$_BDNOMBRE = "foo";
+#En caso de que sea mysql
+#$_BDMOTOR = "MYSQL";
+#$_BDNOMBRE ="";
+#$_BDSERVIDOR ="";
+#$_BDUSUARIO ="";
+#$_BDPASSWORD="";
