989f372e8999ab0653f869a5d97fd089aafa6f66
jrbustosm <jrbustosm@gmail.com>
2013-05-06 17:32:28 -0500

    arreglos menores archivo de configuración

--- conf/conf.php
+++ conf/conf.php
@@ -1,13 +1,24 @@
 <?php
-#configuracion basica
+
+######################
+#configuracion basica#
+######################
+
 $_TITULO = "phantom";
 $_EMAIL = "foo@server";
-#configuracion Base de datos
-$_BDMOTOR = "SQLLITE";
+
+#############################
+#configuracion Base de datos#
+#############################
+
+$_BDMOTOR = "SQLITE";
 $_BDNOMBRE = "foo";
-#En caso de que sea mysql
+
+#En caso de que sea mysql descomentar
 #$_BDMOTOR = "MYSQL";
 #$_BDNOMBRE ="";
 #$_BDSERVIDOR ="";
+#$_BDPUERTO ="";
 #$_BDUSUARIO ="";
 #$_BDPASSWORD="";
+
