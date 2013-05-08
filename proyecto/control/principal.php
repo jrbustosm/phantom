72d341f9e36b53777ba9c6aac71192c5426c3ec7
Yudi Marcela Albarracin <ymarcelaa1509@gmail.com>
2013-05-08 08:28:51 -0500

    Modificación de la clase principal
    Se realizo la modificación de la clase principal para manejar una vista por defecto

--- proyecto/control/principal.php
+++ proyecto/control/principal.php
@@ -1,5 +1,11 @@
 <?php
 class Principal extends Control{
+   public function porDefecto($parametros){
+    $datos= array(
+    "titulo"=>"Mi titulo",
+	  "contenido"=>"Contenido",
+	);
+	$this->mostrarVista("pordefecto", $datos);
+  }
 }  
 
-?>
