08d1b9511d37ef4373022c470582f39e35a60592
Jose Ricardo Bustos Molina <jrbustosm@gmail.com>
2013-05-10 18:43:20 -0500

    me toco subir mostrarVista ..... :(
    quien era el encargado?

--- nucleo/Control.php
+++ nucleo/Control.php
@@ -3,4 +3,8 @@ abstract class Control{
   public function porDefecto($parametros){
     echo 'hola';
   }
+  protected function mostrarVista($vista, $datos){
+    foreach($datos as $llave => $valor) $$llave = $valor;
+    include("proyecto/vista/" . $vista . ".php");
+  }
 }
