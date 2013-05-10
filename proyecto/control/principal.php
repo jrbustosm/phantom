126b047966c8a06cdee3142f5b6e9bb42f73c332
Jose Ricardo Bustos Molina <jrbustosm@gmail.com>
2013-05-10 18:36:03 -0500

    arreglo de estilo

--- proyecto/control/principal.php
+++ proyecto/control/principal.php
@@ -1,12 +1,12 @@
 <?php
 class Principal extends Control{
 	
-	public function porDefecto($parametros){
-	    $datos= array(
-    		"titulo"=>"Mi titulo",
-	  	"contenido"=>"Contenido",
-	);
-	$this->mostrarVista("pordefecto", $datos);
+  public function porDefecto($parametros){
+    $datos= array(
+      "titulo"=>"Mi titulo",
+      "contenido"=>"Contenido",
+    );
+    $this->mostrarVista("pordefecto", $datos);
   }
 }  
 
