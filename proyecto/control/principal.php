22ead6ba20c3e86c41e55115ffdc9d164a8678d7
alejandropirajan <alejandropirajan@gmail.com>
2013-05-10 12:06:52 -0500

    Update principal.php

--- proyecto/control/principal.php
+++ proyecto/control/principal.php
@@ -1,9 +1,10 @@
 <?php
 class Principal extends Control{
-   public function porDefecto($parametros){
-    $datos= array(
-    "titulo"=>"Mi titulo",
-	  "contenido"=>"Contenido",
+	
+	public function porDefecto($parametros){
+	    $datos= array(
+    		"titulo"=>"Mi titulo",
+	  	"contenido"=>"Contenido",
 	);
 	$this->mostrarVista("pordefecto", $datos);
   }
