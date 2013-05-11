1b69841e748c905f5a7d85160de750edb89bd53d
Jeffer1894 <jefer1894@hotmail.com>
2013-05-10 19:33:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include 'nucleo/control.php';
 include 'proyecto/control/principal.php';
 include 'proyecto/control/imagen.php';
+include 'nucleo/Error.php';
 function get_url() {
   $parametros = array();
   $url = parse_url($_SERVER['REQUEST_URI']);
@@ -10,6 +11,16 @@ function get_url() {
   return $parametros;
 }
   $ps= get_url ();
-  print_r($ps);
-  
-
+ 
+  $controlname = $ps[1];//TODO: no siempre esta en 1
+  if(class_exists ($controlname)){
+  $control = new $controlname(); 
+  }else{  
+    $control=new Error();
+  }
+  if($control instanceof Error || count($ps)<=2){
+    $control-> porDefecto($ps);
+  }else{
+    $accion = $ps[2];
+    $control -> $accion($ps);
+  }
