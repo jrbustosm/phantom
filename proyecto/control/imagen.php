b6d7e0c9644de018bfad23c8cd1a1ff5bf030922
ronaldmoreno <rymp_1990@hotmail.com>
2013-05-10 20:26:14 -0500

    imagen.php
    se crea la clase imagen.php la cual se encargara de gestionar las imágenes que se almacenen posteriormente en la base de datos.

---
+++ proyecto/control/imagen.php
@@ -0,0 +1,9 @@
+<?php
+class imagen extends Control{
+  public function porDefecto($parametros){
+		echo "Hola soy imagen por defecto";
+	}
+	public function listar($parametros){
+		echo "Se supone que listo las imagenes";
+	}
+} 
