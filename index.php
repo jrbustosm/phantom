762a04438a16f36410399847753441e18f9d9278
ronaldmoreno <rymp_1990@hotmail.com>
2013-05-10 19:45:39 -0500

    Update index.php
    En esta nueva versión se están manejando los errores por mal ingreso de la url en el index

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 include 'nucleo/control.php';
 include 'proyecto/control/principal.php';
 include 'proyecto/control/imagen.php';
-include 'nucleo/Error.php';
+include 'proyecto/control/Error.php';
+
 function get_url() {
   $parametros = array();
   $url = parse_url($_SERVER['REQUEST_URI']);
@@ -10,17 +11,22 @@ function get_url() {
     if ($p!='') $parametros[] = $p;
   return $parametros;
 }
-  $ps= get_url ();
- 
-  $controlname = $ps[1];//TODO: no siempre esta en 1
-  if(class_exists ($controlname)){
-  $control = new $controlname(); 
-  }else{  
-    $control=new Error();
-  }
-  if($control instanceof Error || count($ps)<=2){
-    $control-> porDefecto($ps);
-  }else{
-    $accion = $ps[2];
-    $control -> $accion($ps);
-  }
+//$control=new Principal();
+//$control->porDefecto(get_url());
+$ps=get_url();
+$controlname=$ps[1]; //TODO: no siempre esta en 1
+if(class_exists($controlname)){
+  $control=new $controlname();
+} else {	
+	$control=new Error();
+   }
+if($control instanceof Error || count($ps)<=2){
+	$control->porDefecto($ps);
+} else {
+	$accion=$ps[2]; //TODO: no siempre esta en 2
+	if(method_exists($control, $accion)){
+		$control->$accion($ps);
+	} else {
+		$control->porDefecto($ps);
+	}
+}
