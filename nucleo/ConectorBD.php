d21e9915321e70e3783579cf8693c0e7ea4530b8
jrbustosm <jrbustosm@gmail.com>
2013-05-11 20:28:06 -0500

    arreglo menores de estilo

--- nucleo/ConectorBD.php
+++ nucleo/ConectorBD.php
@@ -1,13 +1,18 @@
 <?
-  abstract class ConectorBD{
-		protected $manejador;
-			function __construct(){
-				$this->abrir();
-			}
-			function __destruct(){
-				$this->cerrar();
-			}
-			abstract public function abrir();
-			abstract public function cerrar();
-			abstract public function ejecutar($sql);
-	}
+abstract class ConectorBD{
+
+  protected $manejador;
+
+  function __construct(){
+    $this->abrir();
+  }
+
+  function __destruct(){
+    $this->cerrar();
+  }
+
+  abstract public function abrir();
+  abstract public function cerrar();
+  abstract public function ejecutar($sql);
+
+}
--- nucleo/ConectorSQLite.php
+++ nucleo/ConectorSQLite.php
@@ -1,20 +1,24 @@
 <?
-  class conectorSQLite extends ConectorBD{
-		function __construct(){
-			parent::__construct();
-		}
-		function __destruct(){
-			parent::__destruct();
-		}
-		public function abrir(){
-			$this->manejador=sqlite_open("datos/mi.bd");
-		}
-		public function cerrar(){
-			sqlite_close($this->manejador);
-		}
-		public function ejecutar($sql){
-			return sqlite_exec($this->manejador, $sql);
-		}
-		
-	
-	}
+class conectorSQLite extends ConectorBD{
+
+  function __construct(){
+    parent::__construct();
+  }
+
+  function __destruct(){
+    parent::__destruct();
+  }
+
+  public function abrir(){
+    $this->manejador=sqlite_open("datos/mi.bd");
+  }
+
+  public function cerrar(){
+    sqlite_close($this->manejador);
+  }
+
+  public function ejecutar($sql){
+    return sqlite_exec($this->manejador, $sql);
+  }
+
+}
