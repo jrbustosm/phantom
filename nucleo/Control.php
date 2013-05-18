22538cf6cf69d6fb7e9130450f2dc16f8bfd546d
jrbustosm <jrbustosm@gmail.com>
2013-05-18 02:33:36 -0500

    Mejora de comentarios

--- nucleo/Control.php
+++ nucleo/Control.php
@@ -1,34 +1,36 @@
 <?php
 
 /**
-  <p>Clase Control:<br />
-  <br />
-  Maneja las acciones a efectuar de acuerdo a una solicitud,<br/>
-  en teoria los controladores usan los modelos para obtener información<br />
-  pasando esta a las vistas<br />
-  </p>
+ * Control
+ *
+ * @author Jose Ricardo Bustos Molina <jrbustosm@gmail.com>
+ * @link https://github.com/jrbustosm/phantom/blob/master/nucleo/Control.php
+ * @license GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+ * 
+*/
 
-  @abstract
-  @author uniminuto
-  @link https://github.com/jrbustosm/phantom/blob/master/nucleo/Control.php
-  @package nucleo
-  @version 0.1
-  @todo hacer metodo ejecutarAccion para centralizar
+/**
+ * Clase Control
+ * 
+ * Maneja las acciones a efectuar de acuerdo a una solicitud
+ * en teoria los controladores usan los modelos para obtener información
+ * pasando estos datos a las vistas
+ *
+ * @author Jose Ricardo Bustos Molina <jrbustosm@gmail.com>
+ * @version $Id$
+ * @package nucleo
+ * @since 0.1
  */
 abstract class Control{
 
   /**
-    <p>Método ejecutarAccion:<br/>
-    <br/>
-    Ejecutar la accion indicada
-    </p>
-
-    @access public
-    @param string accion Accion a ejecutar
-    @param array parametros Arreglo asociativo con los parametros de la solicitud
-    @return void
+   * Este método Ejecuta la accion indicada, si la accion no existe ejecuta el método
+   * 'accionError', si no se indica ninguna accion ejecuta el método 'porDefecto'
+   *
+   * @param string accion Accion a ejecutar
+   * @param array parametros Arreglo asociativo con los parametros de la solicitud
    */
-  public function ejecutarAccion($accion, $parametros){
+  public function ejecutarAccion($accion="", $parametros=array()){
     if($accion=="") $this->porDefecto($parametros);
     else{
       if(method_exists($this, $accion)){
@@ -40,44 +42,30 @@ abstract class Control{
   }
 
   /**
-    <p>Método porDefecto:<br/>
-    <br/>
-    Método que se ejecuta por defecto si no se indica una acción<br/>
-    </p>
-
-    @access public
-    @param array parametros Arreglo asociativo con los parametros de la solicitud
-    @return void
+   * Método que se ejecuta por defecto si no se indica una acción
+   *
+   * @param array parametros Arreglo asociativo con los parametros de la solicitud
    */
   public function porDefecto($parametros){
     echo 'Hola Mundo!!!';
   }
 
   /**
-    <p>Método accionError:<br/>
-    <br />
-    Método que se ejecuta si se indica una acción inexistente<br />
-    </p>
-
-    @access public
-    @param array parametros Arreglo asociativo con los parametros de la solicitud
-    @return void
+   * Método que se ejecuta si se indica una acción inexistente, por defecto se ejecuta
+   * la acción "porDefecto"
+   *
+   * @param array parametros Arreglo asociativo con los parametros de la solicitud
    */
   public function accionError($parametros){
     $this->porDefecto($parametros);
   }
 
   /**
-    <p>Método mostrarVista:<br/>
-    <br/>
-    Ejecuta una vista determinada, convirtiendo un arreglo asociativo a variables<br/>
+    Ejecuta una vista determinada, convirtiendo un arreglo asociativo a variables
     para que sean mas faciles de usar en la vista
-    </p>
 
-    @access protected
     @param string vista Nombre de la vista a mostrar
     @param array datos Arreglo asociativos con los datos a mostrar en la vista
-    @return void
     @todo mejorar usando templates
    */
   protected function mostrarVista($vista, $datos){
