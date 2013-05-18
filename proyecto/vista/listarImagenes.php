041f6f9e7dd104fb6f347fceb4c4e8068a850ad0
jrbustosm <jrbustosm@gmail.com>
2013-05-17 20:17:20 -0500

    Modificación para el nombre de las clases que heredan de control

--- proyecto/vista/listarImagenes.php
+++ proyecto/vista/listarImagenes.php
@@ -1,10 +1,8 @@
 <?php
-  for($i=0; $i<count($imagenes); ++$i){
-		$nombre = $imagenes[$i]['nombre'];
+for($i=0; $i<count($imagenes); ++$i){
+  $nombre = $imagenes[$i]['nombre'];
 ?>
 <img src = "../img/<?= $nombre?>" style="width:150px; height:200px;"/>
 <?php
-    if($i%5==4) echo "<br />";
-	}
-?>
-		
+  if($i%5==4) echo "<br />";
+}
