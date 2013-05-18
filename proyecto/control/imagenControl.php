8e0032364226dfafe05f1bca8796e46083e3a056
jrbustosm <jrbustosm@gmail.com>
2013-05-18 03:15:57 -0500

    mas sencillo imposible ;)

--- proyecto/control/imagenControl.php
+++ proyecto/control/imagenControl.php
@@ -1,14 +1,13 @@
 <?php
 
 class imagenControl extends Control{
+
   public function porDefecto($parametros){
     $this->listar($parametros);
   }
 
   public function listar($parametros){
-    $datos = array(
-        "imagenes" => Imagen::buscarTodos()
-        );
-    $this->mostrarVista("listarImagenes", $datos);
+    $this->mostrarVista("listarImagenes", ["imagenes" => Imagen::buscarTodos()]);
   }
+
 } 
