3997028cd44570cc23f8eeac8a1a3ce6e53e2e09
Jose Ricardo Bustos Molina <jrbustosm@gmail.com>
2013-05-20 20:22:45 -0500

    Posibilidad de crear instancia de la clase Modelo

--- proyecto/modelo/Imagen.php
+++ proyecto/modelo/Imagen.php
@@ -4,4 +4,13 @@ class Imagen extends Modelo{
   
   const NOMBRETABLA = "imagenes";
 
+  /**
+   * Constructor
+   * 
+   * @param id integer Identificador del registro
+   */
+  function __construct($id){
+    parent::__construct($id);
+  }
+  
 }
